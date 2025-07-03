4e4b75a6fa9ea0293bb53abf12a3397528031ede
Akkshay Sundara Rajan <sundar57@purdue.edu>
2025-07-03 09:29:06 +0530

    Test torch install through poetry

---
+++ src/tinympc/dynamics_model.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+def main():
+    print("Test torch import")
+
+if __name__ == "__main__":
+    main()
