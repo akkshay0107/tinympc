b9999209244e7034eea5005fbc50b1247001a8b8
Akkshay Sundara Rajan <sundar57@purdue.edu>
2025-08-13 14:14:51 +0530

    Changed training hyperparameters

--- python/src/ppo.py
+++ python/src/ppo.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from torch.optim import Adam
+from torch.optim import AdamW
 from torch.distributions import Normal
 import numpy as np
 
@@ -39,7 +39,7 @@ class ValueNet(nn.Module):
         return self.net(obs).squeeze(-1)
 
 class PPOAgent:
-    def __init__(self, env, gamma=0.99, lam=0.95, clip_eps=0.2, lr=3e-4, epochs=10, batch_size=64):
+    def __init__(self, env, gamma=0.99, lam=0.95, clip_eps=0.2, lr=5e-5, epochs=10, batch_size=64):
         self.env = env
         self.gamma = gamma
         self.lam = lam
@@ -49,11 +49,11 @@ class PPOAgent:
 
         self.policy = PolicyNet(self.env.obs_dim, self.env.act_dim)
         self.value = ValueNet(self.env.obs_dim)
-        self.policy_optim = Adam(self.policy.parameters(), lr=lr)
-        self.value_optim = Adam(self.value.parameters(), lr=lr)
+        self.policy_optim = AdamW(self.policy.parameters(), lr=lr)
+        self.value_optim = AdamW(self.value.parameters(), lr=2*lr)
 
     @staticmethod
-    def _score(rewards, var_coef=0.1):
+    def _score(rewards, var_coef=0.05):
         if not rewards:
             return float("-inf")
 
@@ -219,14 +219,14 @@ class PPOAgent:
 def main():
     # constants
     MAX_STEPS = 3000
-    TOTAL_TIMESTEPS = 1_000_000
+    TOTAL_TIMESTEPS = 500_000
 
     env = PyEnvironment(MAX_STEPS)
     agent = PPOAgent(
         env,
         gamma=0.99,
         lam=0.95,
-        clip_eps=0.1,
+        clip_eps=0.2,
         lr=5e-5,
         epochs=10,
         batch_size=64
